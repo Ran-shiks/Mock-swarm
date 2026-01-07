fb371e387887494718b1178a193d4b453f371f0c
francesco <francescoenzoalloro@gmail.com>
2026-01-07 15:08:07 +0100

    solo llm

--- src/generators/__init__.py
+++
@@ -1,5 +0,0 @@
-from .base import BaseGenerator
-from .algorithmic import AlgorithmicGenerator
-from .smart import SmartGenerator
-
-__all__ = ["BaseGenerator", "AlgorithmicGenerator", "SmartGenerator"]
