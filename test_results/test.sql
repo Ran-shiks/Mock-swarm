d376df9ea834a238355c61e4561c06977a1b76fa
francesco <francescoenzoalloro@gmail.com>
2026-01-09 12:01:29 +0100

    finito

---
+++ test_results/test.sql
@@ -0,0 +1,5 @@
+INSERT INTO TEST_TABLE (id, nome, email, voto) VALUES (78, 'solito', 'ermes43@example.net', NULL);
+INSERT INTO TEST_TABLE (id, nome, email, voto) VALUES (12, 'treno', 'mercedes09@example.net', NULL);
+INSERT INTO TEST_TABLE (id, nome, email, voto) VALUES (47, 'memoria', 'monteverdiamadeo@example.com', NULL);
+INSERT INTO TEST_TABLE (id, nome, email, voto) VALUES (27, 'organizzare', 'nzecchini@example.org', NULL);
+INSERT INTO TEST_TABLE (id, nome, email, voto) VALUES (2, 'annunciare', 'ubonatti@example.com', NULL);
